5062af5ad5efa86668f2de4ace9d4f34d7340301
Gustavo Facchinetti <gcfacchi@gmail.com>
2024-08-26 08:21:26 -0300

    E-mail and password validation

--- DesafioBreton/src/app/user-form/user-form.component.ts
+++ DesafioBreton/src/app/user-form/user-form.component.ts
@@ -1,10 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.css',
 })
@@ -12,12 +19,29 @@ export class UserFormComponent {
   userForm: FormGroup;
   constructor(private fb: FormBuilder) {
     this.userForm = this.fb.group({
-      nome: [''],
-      email: [''],
-      senha: [''],
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, this.emailValidator]],
+      senha: ['', [Validators.required, this.senhaValidator]],
     });
   }
   onSubmit() {
     console.log(this.userForm.value);
+  }
+  emailValidator(controle: AbstractControl) {
+    const email = controle.value;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+      return { emailInvalido: true };
+    }
+    return null;
+  }
+
+  senhaValidator(controle: AbstractControl) {
+    const senha = controle.value;
+    const senhaRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    if (!senhaRegex.test(senha)) {
+      return { senhaInvalida: true };
+    }
+    return null;
   }
 }
